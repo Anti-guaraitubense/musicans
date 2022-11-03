5ac2b617742730e0005f782cd9c16403434fe145
GustavoWalk <gustawalk@gmail.com>
2022-11-03 20:42:45 -0300

    pag de instrumentos

---
+++ js/instrumentomusica.js
@@ -0,0 +1,27 @@
+const url_param = new URLSearchParams(window.location.search);
+const inst_param = url_param.get("inst");
+
+if(inst_param === "piano")
+{
+    alert("aa");
+}
+else if(inst_param === "violao")
+{
+    
+}
+else if(inst_param === "bateria")
+{
+
+}
+else if(inst_param === "trompete")
+{
+
+}
+else if(inst_param === "violino")
+{
+
+}
+else if(inst_param === "saxofone")
+{
+
+}
