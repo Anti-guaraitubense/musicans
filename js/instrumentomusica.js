b87e67358547cec5bebe582d838de4b63d756397
GustavoWalk <gustawalk@gmail.com>
2022-11-08 20:18:12 -0300

    dia 8 11 algumas coisas

--- js/instrumentomusica.js
+++ js/instrumentomusica.js
@@ -1,27 +1,52 @@
 const url_param = new URLSearchParams(window.location.search);
 const inst_param = url_param.get("inst");
 
-if(inst_param === "piano")
-{
-    alert("aa");
-}
-else if(inst_param === "violao")
-{
+function inst_atual(){
+    var t1 = document.getElementById("titulo1");
+    var t2 = document.getElementById("titulo2");
+    var t3 = document.getElementById("titulo3");
+    var t4 = document.getElementById("titulo4");
     
-}
-else if(inst_param === "bateria")
-{
-
-}
-else if(inst_param === "trompete")
-{
-
-}
-else if(inst_param === "violino")
-{
-
-}
-else if(inst_param === "saxofone")
-{
-
+    if(inst_param === "piano")
+    {
+        t1.innerHTML = "abacavbvbb";
+        t2.innerHTML = "fdsfdsfds";
+        t3.innerHTML = "fdasfaaa";
+        t4.innerHTML = "aswwwwwww";
+    }
+    else if(inst_param === "violao")
+    {
+        t1.innerHTML = "abacavbvbb";
+        t2.innerHTML = "fdsfdsfds";
+        t3.innerHTML = "fdasfaaa";
+        t4.innerHTML = "aswwwwwww";
+    }
+    else if(inst_param === "bateria")
+    {
+        t1.innerHTML = "abacavbvbb";
+        t2.innerHTML = "fdsfdsfds";
+        t3.innerHTML = "fdasfaaa";
+        t4.innerHTML = "aswwwwwww";
+    }
+    else if(inst_param === "trompete")
+    {
+        t1.innerHTML = "abacavbvbb";
+        t2.innerHTML = "fdsfdsfds";
+        t3.innerHTML = "fdasfaaa";
+        t4.innerHTML = "aswwwwwww";
+    }
+    else if(inst_param === "violino")
+    {
+        t1.innerHTML = "abacavbvbb";
+        t2.innerHTML = "fdsfdsfds";
+        t3.innerHTML = "fdasfaaa";
+        t4.innerHTML = "aswwwwwww";
+    }
+    else if(inst_param === "saxofone")
+    {
+        t1.innerHTML = "abacavbvbb";
+        t2.innerHTML = "fdsfdsfds";
+        t3.innerHTML = "fdasfaaa";
+        t4.innerHTML = "aswwwwwww";
+    }
 }
